5ade8ef8e512d5b2bb14f650d03296ad4d1151fa
Zdenek Pribyla <zdenek.pribyla@gmail.com>
2017-09-22 16:14:20 +0200

    add small tasks

--- app.js
+++ app.js
@@ -1,43 +1,65 @@
-var file = require('./file.json');
-
-
-//console.log(file.length);
-
-
+// var file = require('./file.json');
 //
-var totalHours = 0;
-var workSessions = 0;
-var hoursPerProject = {
-    // projectName: 'test project',
-    // 'Red Bull (V)': 'test 2'
-};
+//
+// //console.log(file.length);
+//
+//
+// //
+// var totalHours = 0;
+// var workSessions = 0;
+// var hoursPerProject = {
+//     // projectName: 'test project',
+//     // 'Red Bull (V)': 'test 2'
+// };
+//
+//
+// for (var i = 0; i < file.length; i++) {
+//
+//     //1. display only unique names of project
+//     // var projectNameList = file[i].project;
+//     // if (uniqueProjectNameList[projectNameList]) {
+//     //     uniqueProjectNameList[projectNameList] = uniqueProjectNameList
+//     // }
+//     // else {
+//     //     uniqueProjectNameList[projectNameList]
+//     // }
+//
+//
+//
+//     //2. display unique names of project and number of working sessions
+//     var projectName = file[i].project;
+//     var workingSessions = file[i];
+//     if (sessionsPerProject[projectName]) {
+//         sessionsPerProject[projectName] = sessionsPerProject[projectName] + workingSessions
+//     }
+//     else {
+//         sessionsPerProject[projectName] = workingSessions + 1;
+//     }
+//
+//
+//     // hoursPerProject - display all projects name and sum of spent hours on every project
+//     // var projectName = file[i].project;
+//     // var hoursSpent =  file[i].hours_spent;
+//     // if (hoursPerProject[projectName]) {
+//     //     hoursPerProject[projectName] = hoursPerProject[projectName] + hoursSpent;
+//     // } else {
+//     //     hoursPerProject[projectName] = hoursSpent;
+//     // }
+//     //
+//     // //display hours and and number of work sessions for Red Bull project
+//     // var object = file[i];
+//     // if (object.project === 'Red Bull (V)') {
+//     //     // console.log(object);
+//     //     // console.log(object.hours_spent);
+//     //     totalHours = totalHours + object.hours_spent;
+//     //     workSessions = workSessions + 1;
+//     // }
+// }
+// // console.log(totalHours);
+// //  console.log(workSessions);
+// // console.log(hoursPerProject);
+// //console.log(uniqueProjectNameList);
+// console.log(sessionsPerProject);
+//
 
 
-
-
-for (var i = 0; i < file.length; i++) {
-
-    //hoursPerProject - display all projects name and sum of spent hours
-    var projectName = file[i].project; // = Red Bull (V)
-    var hoursSpent =  file[i].hours_spent;
-
-    if (hoursPerProject[projectName]) {
-        hoursPerProject[projectName] = hoursPerProject[projectName] + hoursSpent;
-    } else {
-        hoursPerProject[projectName] = hoursSpent;
-    }
-
-    // var object = file[i];
-    //console.log(uniqueName);
-    //
-    // //display hours and and number of work sessions for Red Bull project
-    // if (object.project === 'Red Bull (V)') {
-    //     // console.log(object);
-    //     //console.log(object.hours_spent);
-    //     totalHours = totalHours + object.hours_spent;
-    //     workSessions = workSessions + 1;
-    // }
-}
-//console.log(totalHours);
-//console.log(workSessions);
-console.log(hoursPerProject);
