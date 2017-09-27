87398e2df345474fa6dfa9a082d4ca683237245d
Zdenek Pribyla <zdenek.pribyla@gmail.com>
2017-09-27 16:24:50 +0200

    finish arrays task

--- filter-app.js
+++ filter-app.js
@@ -1,17 +1,23 @@
 var records = require('./file.json');
 
 
-var result = [];
-for (var i = 0; i < records.length; i++) {
-    allRecords = records[i];
-    var objectsName = allRecords.project;
-    var objectSpentHours = allRecords.hours_spent;
-    //console.log(objectsName);
-    var object = {
-        projectName: objectsName,
-        projectHours: objectSpentHours,
-        projectHoursDouble: objectSpentHours * 2
-    };
-    result.push(object)
-}
-console.log(result);
+var modifyData = function (records, multiplier) {
+
+    var results = [];
+    for (var i = 0; i < records.length; i++) {
+        var record = records[i];
+
+        var object = {
+            projectName: record.project,
+            projectHours: multiplier === undefined ? record.hours_spent : record.hours_spent * multiplier
+        };
+
+        results.push(object);
+    }
+    return results;
+};
+
+
+// console.log(modifyData(records, 2));
+console.log(modifyData(records));
+
