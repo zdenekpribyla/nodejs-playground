885a2f0efa8091e54b13db42d868fb80a61c78f2
Zdenek Pribyla <zdenek.pribyla@gmail.com>
2017-09-27 16:25:16 +0200

    add array tasks .map and .filter

---
+++ array-playground.js
@@ -0,0 +1,22 @@
+var myArray = [2, 6, 2, 3, 6, 7, 8, 0, 2];
+
+// ukol 1: vynasov vsechny prvky array dvema
+// ukol 2: vyfiltruj vsechny prvky array ktere jsou vetsi nez 3
+
+var multiplyByTwo = function (number) {
+
+    return number * 2
+};
+
+var higherThanThree = function (number) {
+    return number > 3
+};
+
+console.log(myArray.map(multiplyByTwo));
+console.log(myArray.filter(higherThanThree));
+
+
+
+    // .map()
+    // .filter();
+
