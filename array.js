6726bf1d8d444a3cbcae152ddfba6ec3461d801e
Zdenek Pribyla <zdenek.pribyla@gmail.com>
2017-09-28 12:05:29 +0200

    get unique project name list

--- array.js
+++ array.js
@@ -1,44 +1,44 @@
-var array = [6,4,4,4,9,6,7,6,6,1,'ahoj',false, false];
+var array = [6, 4, 4, 4, 9, 6, 7, 6, 6, 1, 'ahoj', false, false];
 var uniqueArray = [];
 
-for (var i = 0; i < array.length; i++) {
-    // console.log('for loop 1');
-    var item = array[i];
-    //console.log(item);
-
-    if(i === 0) {
-        console.log('i je 0 a proto pushuju prvni objek at je tam aspon jeden');
-        uniqueArray.push(item)
-    }
-
-    console.log('for loop 1 - pracuju na: ' + item);
-
-    var exists = false;
-    // console.log('existuje: ' + exists);
-
-    for (var index = 0; index < uniqueArray.length; index++) {
-        // console.log('for loop 2');
-
-        var item2 = uniqueArray[index];
-        // console.log('for loop 2 - pracuju na: ' + item2);
-
-        //console.log(item2);
-        if (item === item2) {     // go to next number in array ()
-            exists = true
-        }
-        //console.log(uniqueArray);
-    }
-
-    if(exists) {
-        console.log('existuje: ' + exists);
-    } else {
-        uniqueArray.push(item)
-    }
-
-
-
-}
-console.log('uniqueArray: ' + uniqueArray);
+// for (var i = 0; i < array.length; i++) {
+//     // console.log('for loop 1');
+//     var item = array[i];
+//     //console.log(item);
+//
+//     if(i === 0) {
+//         console.log('i je 0 a proto pushuju prvni objek at je tam aspon jeden');
+//         uniqueArray.push(item)
+//     }
+//
+//     console.log('for loop 1 - pracuju na: ' + item);
+//
+//     var exists = false;
+//     // console.log('existuje: ' + exists);
+//
+//     for (var index = 0; index < uniqueArray.length; index++) {
+//         // console.log('for loop 2');
+//
+//         var item2 = uniqueArray[index];
+//         // console.log('for loop 2 - pracuju na: ' + item2);
+//
+//         //console.log(item2);
+//         if (item === item2) {     // go to next number in array ()
+//             exists = true
+//         }
+//         //console.log(uniqueArray);
+//     }
+//
+//     if(exists) {
+//         console.log('existuje: ' + exists);
+//     } else {
+//         uniqueArray.push(item)
+//     }
+//
+//
+//
+// }
+// console.log('uniqueArray: ' + uniqueArray);
 
 // PUSH exercise
 // var emptyArray = [];
@@ -63,10 +63,14 @@ console.log('uniqueArray: ' + uniqueArray);
 // console.log(toUnique(array));
 
 // 3. aproach
-// var unique = array.filter(function(item, i, ar) {
-//      return ar.indexOf(item) === i;
-// });
-// console.log(unique);
+var unique = array.filter(function (item, index, array) {
+    console.log('\n\nitem: ' + item);
+    console.log('i ' + index);
+    console.log('ar ' + array);
+    console.log('array.indexOf(item) ' + array.indexOf(item));
+    return array.indexOf(item) === index;
+});
+console.log(unique);
 
 // // // 4. aproach
 // function uniqueArray2(arr) {
--- map-in-objects.js
+++ map-in-objects.js
@@ -1,6 +1,23 @@
 var records = require('./file.json');
 var _ = require('lodash');
 
+//vylistuj vsechny zaznamy projektu
+var allProjectsNameList = records.map(function (record) {
+   return record.project
+});
+//console.log(allProjectsNameList);
+
+// //udelej seznam projektu - unikatni
+var uniqueProjectnNameList = allProjectsNameList.filter(function (item, index, array) {
+    // console.log('\n\nitem: ' + item);
+    // console.log('i ' + index);
+    // console.log('ar ' + array);
+    // console.log('array.indexOf(item) ' + array.indexOf(item));
+    return array.indexOf(item) === index;
+});
+console.log(uniqueProjectnNameList);
+
+
 //vytahni vsechny optm soft > 3 hodiny
 
 // var hoursSpentMoreThree = records.filter(function (record) {
@@ -9,17 +26,17 @@ var _ = require('lodash');
 
 //console.log(hoursSpentMoreThree);
 
-var onlyOptumSoftHours = records.filter(function (record) {
-    return record.project === 'Red Bull (V)' && record.hours_spent > 3
-});
-
-console.log(onlyOptumSoftHours);
-console.log(onlyOptumSoftHours.length);
-
-var sum = _.sumBy(onlyOptumSoftHours, function (o) {
-    return o.hours_spent;
-});
-console.log(sum);
+// var onlyOptumSoftHours = records.filter(function (record) {
+//     return record.project === 'Red Bull (V)' && record.hours_spent > 3
+// });
+//
+// console.log(onlyOptumSoftHours);
+// console.log(onlyOptumSoftHours.length);
+//
+// var sum = _.sumBy(onlyOptumSoftHours, function (o) {
+//     return o.hours_spent;
+// });
+// console.log(sum);
 
 
 
